c1fd9e45f97f4c793f6303462279e1a60520b044
ammar <ammar.alshareef2014@gmail.com>
2024-10-30 13:42:35 +0200

    ....

--- frontend/src/API/users.js
+++ frontend/src/API/users.js
@@ -1,25 +1,19 @@
 export const signup = async ({ username, email, password }) => {
-  const res = await fetch(
-    `${import.meta.env.VITE_BACKEND_URL}/api/v1/user/signup`,
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, email, password }),
-    },
-  )
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/user/signup`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, email, password }),
+  })
   if (!res.ok) throw new Error('failed to sign up')
   return await res.json()
 }
 
 export const login = async ({ username, password }) => {
-  const res = await fetch(
-    `${import.meta.env.VITE_BACKEND_URL}/api/v1/user/login`,
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password }),
-    },
-  )
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/user/login`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password }),
+  })
   if (!res.ok) throw new Error('failed to login')
   return await res.json()
 }
